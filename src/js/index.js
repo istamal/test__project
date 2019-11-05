b97b5c7d3793c2f689cc33ae497bcf986549f427
istamal <37182977+istamal@users.noreply.github.com>
2019-11-06 01:01:18 +0300

    fixed some bugs

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,22 @@
 
 import '../scss/style.scss';
 
-let buttons = document.getElementsByClassName('text-block__button--raed-more');
+let buttons = document.getElementsByClassName('read-more');
 let hiddens = document.getElementsByClassName('content_hidden');
 let input = document.querySelector('.search-form__input');
 let popupinput = document.querySelector('.popup__input');
-let searchButton = document.querySelector('.search-form__button');
-let popupsearchButton = document.querySelector('.popup__search-button');
-let closeButton = document.querySelector('.side-bar__madal-close-button');
+let searchButton = document.querySelector('.search-form__button--open');
+let popupsearchButton = document.querySelector('.popup__search-form-button--open');
+let closeButton = document.querySelector('.calls-close-button');
 let modalCall = document.querySelector('.side-bar__modal--call');
-let phone = document.querySelector('.contacts__phone');
-let message = document.querySelector('.contacts__message');
+let phone = document.querySelectorAll('.contacts__phone');
+let message = document.querySelectorAll('.contacts__message');
 let modalFeedback = document.querySelector('.side-bar__modal--feedback');
-let feedbackCloseButton = document.querySelector('.side-bar__madal-feedback-close');
+let feedbackCloseButton = document.querySelector('.feedback-close-button');
 let burger = document.querySelector('.side-bar__burger-menu');
 let popup = document.querySelector('.popup');
-let popupClose = document.querySelector('.popup__close-icon');
+let popupClose = document.querySelector('.close-button');
+let wrapper = document.querySelector('.wrapper');
 
 if(window.matchMedia('(max-width: 480px)').matches){
     for (let i = 0; i < hiddens.length; i++ ) {
@@ -45,42 +46,53 @@ if(window.matchMedia('(max-width: 480px)').matches){
 
 
 burger.addEventListener('click', function() {
-    popup.classList.add('show-modal');
+    popup.classList.add('popup-show');
+    wrapper.classList.add('screen-height');
 });
 
 
 popupClose.addEventListener('click', function() {
-    popup.classList.remove('show-modal');
+    popup.classList.remove('popup-show');
 });
 
+function closeModal(button, modal) {
+    button.addEventListener('click', function() {
+        modal.classList.remove('show-modal');
+    });
+}
 
-closeButton.addEventListener('click', function() {
-    modalCall.classList.remove('show-modal');
-});
+closeModal(closeButton, modalCall);
+closeModal(feedbackCloseButton, modalFeedback);
 
-phone.addEventListener('click', function() {
-    modalCall.classList.add('show-modal');
-});
+function openModal(arr, modal) {
+    for (let i = 0; i < arr.length; i++) {
+        arr[i].addEventListener('click', function() {
+            popup.classList.remove('popup-show');
+            modal.classList.add('show-modal');
+            wrapper.classList.add('screen-height');
+        });
+    }
+}
 
-message.addEventListener('click', function() {
-    modalFeedback.classList.add('show-modal');
-});
+openModal(phone, modalCall);
+openModal(message, modalFeedback);
 
-feedbackCloseButton.addEventListener('click', function() {
-    modalFeedback.classList.remove('show-modal');
-});
-
-searchButton.addEventListener('click', function() {
+searchButton.addEventListener('click', function(evt) {
+    evt.preventDefault();
     input.classList.toggle('show');
 });
 
-popupsearchButton.addEventListener('click', function() {
+popupsearchButton.addEventListener('click', function(evt) {
+    evt.preventDefault();
     popupinput.classList.toggle('show');
 });
 
-
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', function() {
-        hiddens[i].classList.toggle('overflow');
-    });
+function showHidden(arr1, arr2) {
+    for (let i = 0; i < arr1.length; i++) {
+        arr1[i].addEventListener('click', function() {
+            arr2[i].classList.toggle('overflow');
+        });
+    }
 }
+
+showHidden(buttons, hiddens);
